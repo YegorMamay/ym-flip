6b5809a2827148ce7ec59e8dd167f8bd11894763
YegorMamay <yegor19.05@gmail.com>
2015-11-20 14:01:45 +0200

    Update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Инструкция:
 1. Подключите jQuery.
 2. Подключите ym-flip.js
-3. Оберните тэг "img" и следующий за ним "p" в "div" c классом flip-right или другим перечисленным ниже, как в примере.
+3. Оберните тэг "img" и следующий за ним "p" в "div" c классом flip-right.
 4. Установите размер изображения, цвет текста и фона через data-атрибуты:
 data-width="250"
 data-height="250"
@@ -12,21 +12,10 @@ data-bg="black"
 Готово!
 
 При наведении курсора на изображения будет срабатывать flip-effect.
-Доступные классы: <br>
-
-flip-right - horizontal right flip <br>
-flip-left - horizontal left flip <br>
-flip-down - vertical down flip <br>
-flip-up - vertical up flip <br>
 
 Пример:
 <div class="flip-right" data-width="250" data-height="250" data-color="white" data-bg="black">
 <img src="http://placehold.it/150x150">
 <p>Some text</p>
 </div>
-
-flip-right<br>
-flip-left<br>
-flip-down<br>
-flip-up<br>
 
--- ym-flip.js
+++ ym-flip.js
@@ -8,14 +8,23 @@ $(function(){
     $(".pic-wrap").css({
         "width": widthRight,
         "height": heightRight,
-        "perspective": "1000px"
+        "perspective": "1000px",
+        "-webkit-perspective": "1000px",
+        "-moz-perspective": "1000px",
+        "-ms-perspective": "1000px",
+        "-o-perspective": "1000px"
         });
     $(".flip-right").css({
         "width": "100%",
         "height": "100%",
         "box-shadow": "0 8px 6px -6px black",
         "transform-style": "preserve-3d",
-        "transition": "all .9s"
+        "-webkit-transform-style": "preserve-3d",
+        "transition": "all .9s",
+        "-webkit-transition": "all .9s",
+        "-moz-transition": "all .9s",
+        "-ms-transition": "all .9s",
+        "-o-transition": "all .9s"
         });
     $(".flip-right img").css({
         "position" : "absolute",
@@ -23,7 +32,15 @@ $(function(){
         "width" : "100%",
         "height" : "100%",
         "backface-visibility":"hidden",
-        "transform": "rotateY(0deg)"
+        "-webkit-backface-visibility":"hidden",
+        "-moz-backface-visibility":"hidden",
+        "-ms-backface-visibility":"hidden",
+        "-o-backface-visibility":"hidden",
+        "transform": "rotateY(0deg)",
+        "-webkit-transform": "rotateY(0deg)",
+        "-moz-transform": "rotateY(0deg)",
+        "-ms-transform": "rotateY(0deg)",
+        "-o-transform": "rotateY(0deg)"
         });
     $(".flip-right p").css({
         "position": "absolute",
@@ -32,144 +49,30 @@ $(function(){
         "color": colorRight,
         "text-align": "center",
         "background-color": bgRight,
-        "transform": "rotateY(180deg)"
+        "transform": "rotateY(180deg)",
+        "-webkit-transform": "rotateY(180deg)",
+        "-moz-transform": "rotateY(180deg)",
+        "-ms-transform": "rotateY(180deg)",
+        "-o-transform": "rotateY(180deg)"
         });
     $(".flip-right").hover(
         function(){
-        $(this).css({"transform": "rotateY(180deg)"});
+        $(this).css({
+            "transform": "rotateY(180deg)",
+            "-webkit-transform": "rotateY(180deg)",
+            "-moz-transform": "rotateY(180deg)",
+            "-ms-transform": "rotateY(180deg)",
+            "-o-transform": "rotateY(180deg)"
+            });
         },
         function(){
-        $(this).css({"transform": "rotateY(0deg)"});
+        $(this).css({
+            "transform": "rotateY(0deg)",
+            "-webkit-transform": "rotateY(0deg)",
+            "-moz-transform": "rotateY(0deg)",
+            "-ms-transform": "rotateY(0deg)",
+            "-o-transform": "rotateY(0deg)"
+            });
         }
     );
-
-
-    var widthLeft = $(".flip-left").data("width");
-    var heightLeft = $(".flip-left").data("height");
-    var colorLeft = $(".flip-left").data("color");
-    var bgLeft = $(".flip-left").data("bg");
-    
-    $(".flip-left").wrap('<div class="pic-wrap"></div>');
-    $(".pic-wrap").css({
-        "width": widthLeft,
-        "height": heightLeft,
-        "perspective": "1000px"
-        });
-    $(".flip-left").css({
-        "width": "100%",
-        "height": "100%",
-        "box-shadow": "0 8px 6px -6px black",
-        "transform-style": "preserve-3d",
-        "transition": "all .9s"
-        });
-    $(".flip-left img").css({
-        "position" : "absolute",
-        "z-index" : "2",
-        "width" : "100%",
-        "height" : "100%",
-        "backface-visibility":"hidden",
-        "transform": "rotateY(0deg)"
-        });
-    $(".flip-left p").css({
-        "position": "absolute",
-        "width": "100%",
-        "height": "100%",
-        "color": colorLeft,
-        "text-align": "center",
-        "background-color": bgLeft,
-        "transform": "rotateY(-180deg)"
-        });
-    $(".flip-left").hover(
-        function(){
-        $(this).css({"transform": "rotateY(-180deg)"});
-        },
-        function(){
-        $(this).css({"transform": "rotateY(0deg)"});
-        });
-
-    var widthDown = $(".flip-down").data("width");
-    var heightDown = $(".flip-down").data("height");
-    var colorDown = $(".flip-down").data("color");
-    var bgDown = $(".flip-down").data("bg");
-    
-    $(".flip-down").wrap('<div class="pic-wrap"></div>');
-    $(".pic-wrap").css({
-        "width": widthDown,
-        "height": heightDown,
-        "perspective": "1000px"
-        });
-    $(".flip-down").css({
-        "width": "100%",
-        "height": "100%",
-        "box-shadow": "0 8px 6px -6px black",
-        "transform-style": "preserve-3d",
-        "transition": "all .9s"
-        });
-    $(".flip-down img").css({
-        "position" : "absolute",
-        "z-index" : "2",
-        "width" : "100%",
-        "height" : "100%",
-        "backface-visibility":"hidden",
-        "transform": "rotateX(0deg)"
-        });
-    $(".flip-down p").css({
-        "position": "absolute",
-        "width": "100%",
-        "height": "100%",
-        "color": colorDown,
-        "text-align": "center",
-        "background-color": bgDown,
-        "transform": "rotateX(-180deg)"
-        });
-    $(".flip-down").hover(
-        function(){
-        $(this).css({"transform": "rotateX(-180deg)"});
-        },
-        function(){
-        $(this).css({"transform": "rotateX(0deg)"});
-        });
-    
-    var widthUp = $(".flip-up").data("width");
-    var heightUp = $(".flip-up").data("height");
-    var colorUp = $(".flip-up").data("color");
-    var bgUp = $(".flip-up").data("bg");
-    
-    $(".flip-up").wrap('<div class="pic-wrap"></div>');
-    $(".pic-wrap").css({
-        "width": widthUp,
-        "height": heightUp,
-        "perspective": "1000px"
-        });
-    $(".flip-up").css({
-        "width": "100%",
-        "height": "100%",
-        "box-shadow": "0 8px 6px -6px black",
-        "transform-style": "preserve-3d",
-        "transition": "all .9s"
-        });
-    $(".flip-up img").css({
-        "position" : "absolute",
-        "z-index" : "2",
-        "width" : "100%",
-        "height" : "100%",
-        "backface-visibility":"hidden",
-        "transform": "rotateX(0deg)"
-        });
-    $(".flip-up p").css({
-        "position": "absolute",
-        "width": "100%",
-        "height": "100%",
-        "color": colorUp,
-        "text-align": "center",
-        "background-color": bgUp,
-        "transform": "rotateX(180deg)"
-        });
-    $(".flip-up").hover(
-        function(){
-        $(this).css({"transform": "rotateX(180deg)"});
-        },
-        function(){
-        $(this).css({"transform": "rotateX(0deg)"});
-        });
 });
